a9480e2fc71e9e824ae81966a4f3cd8249a45908
SACHIN KUMAR <145637427+sachin9525@users.noreply.github.com>
2026-04-27 12:46:57 +0530

    Refactor error handling

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 
 const app = express();
-const { adminAuth, userAuth } = require("./middlewares/auth")
 
-app.use("/admin", adminAuth)
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data Sent")
+app.use("/", (err, res, req, next) => {
+  console.log("Nothing error 2")
+  if(err){
+    res.status(500).send("Somthing went ")
+  }
 })
 
-app.get("/user", userAuth, (req, res) => {
-  res.send("Valid User Id")
+
+app.get("/user", (req, res) => {
+ try{
+    console.log("Nothing error")
+    throw new error("error somying")
+    res.send("User data sent")
+  }
+  catch(err){
+    res.status(500).send("Somthing went wrong, contact support team")
+  }
 })
+
 
 app.listen(7777, () => {
   console.log("Server running on port 7777");
