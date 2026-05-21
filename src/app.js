e28ad8bd89b20057fb0de05d7709f7eb25214ce1
SACHIN KUMAR <145637427+sachin9525@users.noreply.github.com>
2026-05-21 11:56:46 +0530

    cors origin and login page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const mongoDB = require("./config/database");
 const cookieParser = require("cookie-parser");
+const cors = require("cors")
 
 const app = express();
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials:true
+}))
 app.use(express.json());
 app.use(cookieParser());
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -50,7 +50,7 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
       });
-      res.send("Login Seccessful🫡!!!");
+      res.send(user);
     } else {
       throw new Error("Inavalid Credentials😑");
     }
